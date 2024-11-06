7b73312dbb8fbb442333bf78eacec6f9b408ebc3
mtchun1 <mtchun2@gmail.com>
2024-11-06 14:55:30 -0800

    started lif

---
+++ src/lif.v
@@ -0,0 +1,21 @@
+default_nettype none
+
+module lif {
+    input wire [0:7]    current,
+    input wire          clk,
+    input wire          reset_n,
+    input reg  [0:7]    state,
+    output wire         spike,
+}
+    wire [0:7] next_state;
+    reg [0:7] threshold;
+
+    always @(posedge clk) begin
+        if (!reset_n) begin
+            state <= 0;
+            threshold <= 200;
+        end else begin
+            state <= next_state;
+        end
+    end
+endmodule
