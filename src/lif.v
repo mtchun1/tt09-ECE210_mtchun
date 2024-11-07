c11a9847aa2fb70654f16599a11987f98d8712d0
Michael Chun <mtchun2@gmail.com>
2024-11-06 20:31:02 -0800

    changed curly to regular

--- src/lif.v
+++ src/lif.v
@@ -1,17 +1,17 @@
 `default_nettype none
 
-module lif {
+module lif (
     input wire [0:7]    current,
     input wire          clk,
     input wire          reset_n,
     input reg  [0:7]    state,
     output wire         spike
-};
+);
     wire [0:7] next_state;
     reg [0:7] threshold;
 
     always @(posedge clk) begin
-        
+
         if (!reset_n) begin
             state <= 0;
             threshold <= 200;
