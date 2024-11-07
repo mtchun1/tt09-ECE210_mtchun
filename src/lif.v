f8dfe3bdedcba916326b87ab4dd6a0f74864cc53
Michael Chun <mtchun2@gmail.com>
2024-11-06 20:15:15 -0800

    added lif code

--- src/lif.v
+++ src/lif.v
@@ -5,8 +5,8 @@ module lif {
     input wire          clk,
     input wire          reset_n,
     input reg  [0:7]    state,
-    output wire         spike,
-}
+    output wire         spike
+};
     wire [0:7] next_state;
     reg [0:7] threshold;
 
@@ -18,4 +18,10 @@ module lif {
             state <= next_state;
         end
     end
+
+    // next state logic
+    assign next_state = current + (state << 1);
+
+    // spiking logic
+    assign spike = (state >= threshold);
 endmodule
